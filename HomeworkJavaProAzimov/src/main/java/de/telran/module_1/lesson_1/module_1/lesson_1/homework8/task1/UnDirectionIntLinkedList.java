d18e3c13929a4fbb80d7d98ef91994aebc79aa60
Anvarjon <142890754+Anvarjon7@users.noreply.github.com>
2023-12-07 21:39:40 +0100

    Delete HomeworkJavaProAzimov/src/main/java/de/telran/module_1/lesson_1/module_1/lesson_1/homework8 directory

--- HomeworkJavaProAzimov/src/main/java/de/telran/module_1/lesson_1/module_1/lesson_1/homework8/task1/UnDirectionIntLinkedList.java
+++
@@ -1,2 +0,0 @@
-package de.telran.module_1.lesson_1.module_1.lesson_1.homework8.task1;public class UnidirectionIntLinkedList {
-}
--- HomeworkJavaProAzimov/src/main/java/de/telran/module_1/lesson_1/module_1/lesson_1/homework8/task2/SimpleLinkedList.java
+++
@@ -1,2 +0,0 @@
-package de.telran.module_1.lesson_1.module_1.lesson_1.homework8.task2;public class SimpleLinkedList {
-}
